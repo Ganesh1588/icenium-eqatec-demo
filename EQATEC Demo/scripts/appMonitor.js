52285dfd7a67917bb813d0b40661d61d74dfc1f7
Todd Anglin <toddanglin@gmail.com>
2013-09-26 22:02:23 -0500

    Added script to manage EQATEC logging actions

---
+++ EQATEC Demo/scripts/appMonitor.js
@@ -0,0 +1,64 @@
+(function (global) {
+    var monitor,
+        app = global.app = global.app || {};
+    
+    document.addEventListener("unload", app.stopMonitor);
+    window.addEventListener("beforeunload", app.stopMonitor);
+    
+    app.stopMonitor = function(e){      
+        //Stop monitor if running
+        console.log("Stopping monitor...");
+        if(monitor){
+            monitor.stop();
+        }
+    };
+    
+    app.startMonitor = function(){
+        if (monitor)   
+            return;        
+        
+        try    
+        {
+            // This application is using EQATEC Analytics    
+            // See the documentation at http://www.telerik.com/analytics/resources/documentation    
+            // Create the monitor instance using the unique product key for Test App
+            
+            var settings = _eqatec.createSettings("07C10A76305040B1A2E87D83A56C170B");
+            
+            settings.version = "1.2.3";
+            
+            monitor = _eqatec.createMonitor(settings);
+            console.log("Monitor created", monitor);
+            
+            
+            // Start the monitor when your application starts    
+            monitor.start();
+            console.log("Monitor started");
+            
+        }    
+        catch (e)    
+        {    
+            console.log("EQATEC Analytics exception: " + e.description);    
+        }    
+    };
+  
+    app.logViewStart = function(e){
+     	//Log the view show to our app analytics
+        
+        var currentView = "View."+ e.view.title;
+        console.log("Log feature", currentView);
+        
+        monitor.trackFeature(currentView);
+        
+        //Start view timer
+        monitor.trackFeatureStart(currentView);
+    };
+
+    app.logViewEnd = function(e){
+        var currentView = "View."+ e.view.title;
+        console.log("End timing for feature", currentView);
+        
+        monitor.trackFeatureStop(currentView);
+    }
+    
+})(window);
